1016eeba6f531b8f6a6f45bd02d195eb218795bd
Sirius Kim <jmoumnet@gmail.com>
2025-10-03 23:37:32 +0900

    Initial Commit

---
+++ Chokotalk/ChokotalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChokotalkApp.swift
+//  Chokotalk
+//
+//  Created by JSS_MacbookPro on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ChokotalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
